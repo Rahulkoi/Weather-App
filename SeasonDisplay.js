b9ad579d74327d0d751b0bb700fcbe1e4fd19cb1
Rahul <92011503+Rahulkoi@users.noreply.github.com>
2022-09-15 14:30:34 +0530

    Add files via upload

---
+++ SeasonDisplay.js
@@ -0,0 +1,26 @@
+import './SeasonDisply.css';
+import React from "react";
+
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const text = season === "winter" ? "burr its freezing" : "Lets hit the beach";
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  return (
+    <div className={`season-display ${season}`}>
+    <i className={`icon-left massive ${icon} icon`}/>
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${icon} icon`}/>
+    </div>
+  );
+};
+
+export default SeasonDisplay;
